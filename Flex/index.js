1d22f7d6b85f63f1f8e063bc5f5ca183519ef985
Tessa van Elderen <90682645+TessavanElderen@users.noreply.github.com>
2021-12-03 15:02:19 +0100

    Les 7 afgerond

--- Flex/index.js
+++ Flex/index.js
@@ -21,17 +21,64 @@ class game
 {
     constructor()
     {
-        this.selectplayerDiv = document.getElementsByClassName("selectplayer"); 
-        this.winnerDiv = document.getElementsByClassName("winner");
-        this.playerturnDiv = document.getElementsByClassName("playerturn");
-        this.rollDiv = document.getElementsByClassName("roll");
-        this.mainDiv = document.getElementsByClassName("main");
-        this.boardDiv = document.getElementsByClassName("board");
-        this.boardoverlayDiv = document.getElementsByClassName("boardoverlay");
+        this.selectplayerDiv = document.getElementsByClassName("selectplayer")[0]; 
+        this.winnerDiv = document.getElementsByClassName("winner")[0];
+        this.playerturnDiv = document.getElementsByClassName("playerturn")[0];
+        this.rollDiv = document.getElementsByClassName("roll")[0];
+        this.mainDiv = document.getElementsByClassName("main")[0];
+        this.boardDiv = document.getElementsByClassName("board")[0];
+        this.boardoverlayDiv = document.getElementsByClassName("boardoverlay")[0];
 
         this.tile = []
         this.player = []
         this.playerTurn = (0)
-
     }
+
+setupBoard()
+{
+
 }
+
+setupGoto()
+{
+
+} 
+
+start(amountOfPlayer)
+{
+   
+}
+
+moveToNextPlayer()
+{
+
+}
+
+draw()
+{
+
+}
+
+roll()
+{
+
+}
+
+setPawn()
+{
+
+}
+
+makeBoardDiv(x, y, tileDisplayNumber)
+{
+
+}
+
+}
+
+function init()
+{
+    game = new Game(); 
+}
+    let game;
+
