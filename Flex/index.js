97b49f29f7a7a46a78186b9d26731416aea16a86
Tessa van Elderen <90682645+TessavanElderen@users.noreply.github.com>
2021-12-03 14:44:45 +0100

    Les 6 afgemaakt

--- Flex/index.js
+++ Flex/index.js
@@ -1,5 +1,11 @@
 class player{
-
+    constructor(index)
+    {
+        this.index = index; 
+        this.atTile = 0;
+        this.pawn = document.getElementsByClassName("pawn" + index)[0];  
+        this.pawn.style.display = "block"; 
+    }
 }
 
 class tile
@@ -7,16 +13,25 @@ class tile
     constructor(div)
     {
         this.div = div; 
-        let className = document.getElementsByClassName("board")[0];
-        className.style.display = "block"
-        this.goto = -1; 
-          
-         
+        this.goto = -1;
     }
 }
 
 class game
 {
+    constructor()
+    {
+        this.selectplayerDiv = document.getElementsByClassName("selectplayer"); 
+        this.winnerDiv = document.getElementsByClassName("winner");
+        this.playerturnDiv = document.getElementsByClassName("playerturn");
+        this.rollDiv = document.getElementsByClassName("roll");
+        this.mainDiv = document.getElementsByClassName("main");
+        this.boardDiv = document.getElementsByClassName("board");
+        this.boardoverlayDiv = document.getElementsByClassName("boardoverlay");
 
+        this.tile = []
+        this.player = []
+        this.playerTurn = (0)
+
+    }
 }
-
