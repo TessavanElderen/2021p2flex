33f5f348acaa0ac21cd1c803b1646240c6e5dad7
Tessa van Elderen <90682645+TessavanElderen@users.noreply.github.com>
2021-12-03 13:53:21 +0100

    nieuwe foto's toegevoegd

---
+++ Flex/index.js
@@ -0,0 +1,22 @@
+class player{
+
+}
+
+class tile
+{
+    constructor(div)
+    {
+        this.div = div; 
+        let className = document.getElementsByClassName("board")[0];
+        className.style.display = "block"
+        this.goto = -1; 
+          
+         
+    }
+}
+
+class game
+{
+
+}
+
