85b7af900bce46145c197e4653d4584139fd1970
Tessa van Elderen <90682645+TessavanElderen@users.noreply.github.com>
2021-12-09 14:06:50 +0100

    les 9 (bord doet het alleen nog niet)

--- Flex/index.js
+++ Flex/index.js
@@ -1,4 +1,13 @@
-class player{
+
+let game;
+
+function init()
+{
+    game = new Game(); 
+}
+    
+class player
+{
     constructor(index)
     {
         this.index = index; 
@@ -17,6 +26,9 @@ class tile
     }
 }
 
+
+
+
 class game
 {
     constructor()
@@ -29,56 +41,91 @@ class game
         this.boardDiv = document.getElementsByClassName("board")[0];
         this.boardoverlayDiv = document.getElementsByClassName("boardoverlay")[0];
 
-        this.tile = []
-        this.player = []
-        this.playerTurn = (0)
+        this.tile = [];
+        this.player = [];
+        this.playerTurn = 0;
+        this.setupBoard();
     }
 
-setupBoard()
-{
+    setupBoard()
+    {
+        let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
+            0, 3, 3, 3, 3, 3, 3, 3, 3, 0,
+            0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
+            0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 
+            0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+        let x = 0;
+        let y = 10;
+        let tileSize = 55;
+
+
+        for (var i = 0; i < path.length; i++)
+        {
+            let cmd = path[i];
+            if (cmd == 1)
+            {
+                x++;
+            }
+            else if (cmd == 3)
+            {
+                x--;
+            }
+            else if (cmd == 0)
+            {
+                y--;
+            }
+        }
+        let div = this.makeBoardDiv(x * tileSize, y * tileSize, i + 1)
+
+        let tile = new Tile(div);
+        this.tiles.push(tile); 
+    
+    } 
+
+  
+    setupGoto()
+    {
+
+    } 
+
+    start(amountOfPlayer)
+    {
+    
+    }
+
+    moveToNextPlayer()
+    {
+
+    }
+
+    draw()
+    {
+
+    }
+
+    roll()
+    {
+
+    }
+
+    setPawn(playerI, atTile)
+    {
+
+    }
+
+    makeBoardDiv(x, y, tileDisplayNumber)
+    {
+        let div = document.createElement("div");
+        
+        div.className = "tile"; 
+        div.style.left = x + "px";
+        div.style.top = y + "px";
+        div.textContent = tileDisplayNumber; 
+
+        this.boardDiv.appendChild(div);
+
+        return div; 
+    }
 
 }
-
-setupGoto()
-{
-
-} 
-
-start(amountOfPlayer)
-{
-   
-}
-
-moveToNextPlayer()
-{
-
-}
-
-draw()
-{
-
-}
-
-roll()
-{
-
-}
-
-setPawn()
-{
-
-}
-
-makeBoardDiv(x, y, tileDisplayNumber)
-{
-
-}
-
-}
-
-function init()
-{
-    game = new Game(); 
-}
-    let game;
-
